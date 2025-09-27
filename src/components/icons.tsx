4dec6faa100ec903df4a5d9b97113474da6498d0
Caterina Mansilla <catimansilla94@gmail.com>
2025-09-27 15:27:19 +0000

    que este sea el icono de la aplicacion: Rosario remAI

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,45 +4,50 @@ export function BridgeIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
-      width="24"
-      height="24"
-      viewBox="0 0 59.27 59.27"
+      viewBox="0 0 200 100"
       xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
     >
-      <g transform="translate(-2.65 -2.65)">
-        <path
-          d="m21.17 21.17-8.47 23.81h-2.65v-2.65h-2.64v-2.64h-2.65v-2.65h-2.11v-2.64h2.11v-2.65h2.65v-2.64h2.64v-2.65h2.65zm5.29 0-10.58 23.81h-2.65v-2.65h-2.64v-2.64h-2.65v-2.65h-2.11v-2.64h4.76v-2.65h2.65v-2.64h2.64v-2.65h2.65zm-15.88 23.81h-2.64v-2.65h-2.65v-2.64h-2.64v-2.65h-2.12v5.29h2.12v2.65h2.64v2.64h2.65v2.65h2.64v-2.65h.53v-2.64h-.53z"
-          fill="currentColor"
-        />
-        <path
-          d="m3.18 45.51 2.11-5.29-2.11-5.29h55.96v10.58zm50.67-2.64h2.65v-2.65h-2.65z"
-          fill="currentColor"
-        />
-        <path
-          d="m31.75 5.29-2.65 31.75h5.29L31.75 5.29zm2.64 31.75h-5.29l2.64-31.75 2.65 31.75z"
-          fill="currentColor"
-        />
-        <path
-          d="m58.74 37 2.64-15.88-2.64-15.87-2.65 15.87zm-5.29-15.87-2.65 15.87 2.65 15.88-2.65-15.88z"
-          fill="currentColor"
-        />
-        <path
-          d="m53.45 21.17-2.64 15.87h-5.3l-2.64 15.87h-2.65l2.65-15.87h-5.29l2.64-15.87h5.3l2.64-15.88h2.64l-2.64 15.88h5.3zm-7.94 15.87h-2.64l-2.65 15.87h-2.65l5.29-15.87z"
-          fill="currentColor"
-        />
-        <path
-          d="m3.18 45.51h55.96v-10.58h-55.96v10.58m53.32-7.94h-50.67v5.29h50.67v-5.29"
-          fill="currentColor"
-        />
-        <path
-          d="m34.39 37h23.81v-15.88h-23.81v15.88m21.17-13.23h-18.52v10.58h18.52v-10.58"
-          fill="currentColor"
-        />
-        <path
-          d="M31.75 5.29 5.82 45.51h5.29L31.75 13.23l26.46 32.28h5.29L31.75 5.29z"
-          fill="currentColor"
-        />
-      </g>
+      <path
+        fill="currentColor"
+        d="M0 70 L200 70 L200 75 L0 75 Z"
+      />
+      <path
+        fill="currentColor"
+        d="M50 20 L55 20 L55 70 L50 70 Z"
+      />
+      <path
+        fill="currentColor"
+        d="M145 20 L150 20 L150 70 L145 70 Z"
+      />
+      <path
+        fill="currentColor"
+        d="M52.5 20 L90 70 L87 70 L52.5 25 Z"
+      />
+      <path
+        fill="currentColor"
+        d="M52.5 20 L15 70 L12 70 L52.5 25 Z"
+      />
+      <path
+        fill="currentColor"
+        d="M147.5 20 L185 70 L182 70 L147.5 25 Z"
+      />
+      <path
+        fill="currentColor"
+        d="M147.5 20 L110 70 L107 70 L147.5 25 Z"
+      />
+      <path
+        fill="currentColor"
+        d="M52.5,15 L52.5,20 L90,70 L92,70 L55,15 L52.5,15 Z M147.5,15 L147.5,20 L110,70 L112,70 L150,15 L147.5,15 Z"
+      />
+       <path
+        fill="currentColor"
+        d="M55,20 L50,20 L15,70 L20,70 L55,20 Z M145,20 L150,20 L185,70 L180,70 L145,20 Z"
+      />
+       <path
+        fill="currentColor"
+        d="M0 75 L200 75 L200 85 Q 150 95, 100 85 Q 50 95, 0 85 Z"
+      />
     </svg>
   );
 }
